21570c4ac869e2b19f0fc7cf575b0edd19db7ecc
martina-png <84143039+martina-png@users.noreply.github.com>
2021-05-28 11:07:53 -0300

    Add files via upload

---
+++ Ejercicio1_puerto_serial.ino/Ejercicio1_puerto_serial.ino.ino
@@ -0,0 +1,15 @@
+void setup(){
+Serial.begin(9600);
+}
+
+void loop(){
+ 
+}
+
+void serialEvent(){
+   if (Serial.available()){
+     char x = Serial.read();  
+     Serial.println("Se ingresaron datos"); 
+ }
+}
+
---
+++ Ejercicio2_puerto_serial.ino/Ejercicio2_puerto_serial.ino.ino
@@ -0,0 +1,17 @@
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+}
+
+void serialEvent() { //esta función se activa cuando hay datos disponibles en el puerto serial
+
+ String lectura = Serial.readString(); //almacenar la lectura del buffer del serial en la variable "lectura"
+Serial.print("Se ingreso: ");
+Serial.print(lectura);
+Serial.print(" con ");
+Serial.print(lectura.length());
+Serial.println(" caracteres.");
+}
+
