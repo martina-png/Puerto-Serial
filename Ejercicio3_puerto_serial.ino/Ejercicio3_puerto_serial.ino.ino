9ada4c69974808820a2481e80b98c8d3f629acbf
martina-png <84143039+martina-png@users.noreply.github.com>
2021-05-28 11:37:12 -0300

    Add files via upload

---
+++ Ejercicio3_puerto_serial.ino/Ejercicio3_puerto_serial.ino.ino
@@ -0,0 +1,16 @@
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("FUNCIONAMIENTO: OK.");
+  delay(1000);
+}
+
+void serialEvent(){ 
+ String lectura = Serial.readString();
+  
+  if(lectura.equals("hola")){
+   Serial.println("Full picado pa"); 
+  }
+}
